2d51961ecf5b3250eaa833169d0c2ce07869b2a6
Daniel Calviño Sánchez <danxuliu@gmail.com>
2023-08-16 11:23:03 +0200

    fixup! Fix session ID not updated in the local model after forced reconnections
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

--- src/components/CallView/CallView.vue
+++ src/components/CallView/CallView.vue
@@ -365,7 +365,7 @@ export default {
 		'localCallParticipantModel.attributes.peerId'(newValue, previousValue) {
 			const index = this.screens.indexOf(previousValue)
 			if (index !== -1) {
-				this.screens.splice(index, 1, newValue)
+				this.screens[index] = newValue
 			}
 		},
 
